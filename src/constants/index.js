22928d298da61644a23adc0e6945dff5bec56297
rajatmaini <37542727+rajatmaini@users.noreply.github.com>
2023-05-14 03:28:05 +0530

    update

--- src/constants/index.js
+++ src/constants/index.js
@@ -183,7 +183,7 @@ import {
           color: "pink-text-gradient",
         },
       ],
-      image: "./src/assets/Cast_our_Vote.png",
+      image: "./public/Cast_your_Vote.png",
       source_code_link: "https://github.com/",
     },
     {
