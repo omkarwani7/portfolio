b66eaed96788ffdeda6b24855d65386d1cf6b993
Omkar Wani <omkar.wani55@gmail.com>
2023-05-14 20:34:36 +0530

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
           <div className="w-1 sm:h-80 h-40 violet-gradient"/>
         </div>
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Rajat Maini</span></h1>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Omkar Wani</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
            I develop Android and Web
            <br className="sm:block hidden"/>
